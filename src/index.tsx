3704349c641cac426e305951ef3649e948c6c067
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2022-03-18 15:39:25 +0800

    fixed jsonld base

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,56 @@
-export { default as DefaultSeo } from './meta/defaultSEO';
-export { default as NextSeo } from './meta/nextSEO';
-
+export { ArticleJsonLdProps, default as ArticleJsonLd } from './jsonld/article';
+export { BrandJsonLdProps, default as BrandJsonLd } from './jsonld/brand';
 export {
-  default as CarouselJsonLd,
+  BreadCrumbJsonLdProps,
+  default as BreadcrumbJsonLd,
+} from './jsonld/breadcrumb';
+export {
   CarouselJsonLdProps,
+  default as CarouselJsonLd,
 } from './jsonld/carousel';
+export {
+  CollectionPageJsonLdProps,
+  default as CollectionPageJsonLd,
+} from './jsonld/collectionPage';
+export {
+  CorporateContactJsonLdProps,
+  default as CorporateContactJsonLd,
+} from './jsonld/corporateContact';
+export { CourseJsonLdProps, default as CourseJsonLd } from './jsonld/course';
+export { DatasetJsonLdProps, default as DatasetJsonLd } from './jsonld/dataset';
+export { default as EventJsonLd, EventJsonLdProps } from './jsonld/event';
+export { default as FAQPageJsonLd, FAQPageJsonLdProps } from './jsonld/faqPage';
+export {
+  default as JobPostingJsonLd,
+  JobPostingJsonLdProps,
+} from './jsonld/jobPosting';
+export { JsonLd, JsonLdProps } from './jsonld/jsonld';
+export {
+  default as LocalBusinessJsonLd,
+  LocalBusinessJsonLdProps,
+} from './jsonld/localBusiness';
+export { default as LogoJsonLd, LogoJsonLdProps } from './jsonld/logo';
 export {
   default as NewsArticleJsonLd,
   NewsArticleJsonLdProps,
 } from './jsonld/newsarticle';
 export {
-  default as JobPostingJsonLd,
-  JobPostingJsonLdProps,
-} from './jsonld/jobPosting';
-export {
-  default as LocalBusinessJsonLd,
-  LocalBusinessJsonLdProps,
-} from './jsonld/localBusiness';
-export { default as QAPageJsonLd, QAPageJsonLdProps } from './jsonld/qaPage';
+  default as OrganizationJsonLd,
+  OrganizationJsonLdProps,
+} from './jsonld/organization';
+export { default as PersonJsonLd, PersonJsonLdProps } from './jsonld/person';
+export { default as ProductJsonLd, ProductJsonLdProps } from './jsonld/product';
 export {
   default as ProfilePageJsonLd,
   ProfilePageJsonLdProps,
 } from './jsonld/profilePage';
+export { default as QAPageJsonLd, QAPageJsonLdProps } from './jsonld/qaPage';
+export { default as RecipeJsonLd, RecipeJsonLdProps } from './jsonld/recipe';
 export {
   default as SiteLinksSearchBoxJsonLd,
   SiteLinksSearchBoxJsonLdProps,
 } from './jsonld/siteLinksSearchBox';
-export { default as RecipeJsonLd, RecipeJsonLdProps } from './jsonld/recipe';
-export { default as EventJsonLd, EventJsonLdProps } from './jsonld/event';
-export {
-  default as CorporateContactJsonLd,
-  CorporateContactJsonLdProps,
-} from './jsonld/corporateContact';
-export {
-  default as CollectionPageJsonLd,
-  CollectionPageJsonLdProps,
-} from './jsonld/collectionPage';
-export { default as ProductJsonLd, ProductJsonLdProps } from './jsonld/product';
-export { default as PersonJsonLd, PersonJsonLdProps } from './jsonld/person';
+export { default as SocialProfileJsonLd } from './jsonld/socialProfile';
 export {
   default as SoftwareAppJsonLd,
   SoftwareAppJsonLdProps,
@@ -47,23 +60,8 @@ export {
   default as VideoGameJsonLd,
   VideoGameJsonLdProps,
 } from './jsonld/videoGame';
-export {
-  default as OrganizationJsonLd,
-  OrganizationJsonLdProps,
-} from './jsonld/organization';
-export { default as FAQPageJsonLd, FAQPageJsonLdProps } from './jsonld/faqPage';
-export { default as LogoJsonLd, LogoJsonLdProps } from './jsonld/logo';
-export { default as DatasetJsonLd, DatasetJsonLdProps } from './jsonld/dataset';
-export { default as CourseJsonLd, CourseJsonLdProps } from './jsonld/course';
-export {
-  default as BreadcrumbJsonLd,
-  BreadCrumbJsonLdProps,
-} from './jsonld/breadcrumb';
-export { default as BrandJsonLd, BrandJsonLdProps } from './jsonld/brand';
-export { default as ArticleJsonLd, ArticleJsonLdProps } from './jsonld/article';
 export { default as WebPageJsonLd, WebPageJsonLdProps } from './jsonld/webPage';
-export { default as SocialProfileJsonLd } from './jsonld/socialProfile';
-
+export { default as DefaultSeo } from './meta/defaultSEO';
+export { default as NextSeo } from './meta/nextSEO';
 export * from './types';
 export * from './utils/schema/index';
-export * from './utils/toJson';
